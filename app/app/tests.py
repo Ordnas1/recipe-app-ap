40f598e85444a62382f814def0979b06554a374c
Ordnas1 <sandromarcelo.peirano@gmail.com>
2022-07-07 17:35:35 -0500

    Implement wait_for_db command

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,9 +1,10 @@
-""" 
-Sample test 
+"""
+Sample test
 """
 from django.test import SimpleTestCase
 
 from app import calc
+
 
 class CalcTests(SimpleTestCase):
     "Test teh calc module"
@@ -13,7 +14,7 @@ class CalcTests(SimpleTestCase):
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
         """Test substracting number"""
         res = calc.subtract(10, 15)
